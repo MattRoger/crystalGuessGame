c13d31f1e6dacc8bd2f6737393e82bc66f5fae9f
MattRoger <mrogerphotography@gmail.com>
2019-09-10 22:06:33 -0600

    added crystal image array

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,23 @@ var score=0;
 
 var numberOptions = [10, 5, 3, 7];
 
-for (var i = 0; i < numberOptions.length; i++){
+for (var i = 0; i < 4; i++){
     var imageCrystal = $("<img>");
+    var crystalArray=[
+        "assets/images/cr1_bl.png",
+        "assets/images/cr1_grn.png",
+        "assets/images/cr1_purple.png",
+        "assets/images/cr1_red.png",
+        "assets/images/cr2_bl.png",
+        "assets/images/cr2_grn.png",
+        "assets/images/cr2_purple.png",
+        "assets/images/cr2_red.png",
+        
+    ]
+    // var randomCrystal = crystalArray[Math.floor(Math.random()*crystalArray/length)];
+
     imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "assets/images/cr1_bl.png");
+    imageCrystal.attr("src", crystalArray[Math.floor(Math.random()*crystalArray.length)] );
     
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
     $("#crystals").append(imageCrystal);
